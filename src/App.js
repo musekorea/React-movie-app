0e61483ace1507a584c7e8aa7c8ecb79b3ef38d0
musekorea <musekorea@hanmail.net>
2021-11-24 19:31:34 +0800

    fetch data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { LITERAL_TYPES } from '@babel/types';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const getMovies = async () => {
+    try {
+      const movieRaws = await fetch(
+        `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
+      );
+      const movieJson = await movieRaws.json();
+      setMovies(movieJson.data.movies);
+      setIsLoading(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    getMovies();
+  }, []);
+  console.log(movies);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>REACT MOVIE APP</h1>
+      <strong>{isLoading ? '⏰ Now is loading, please wait!!' : null}</strong>
+      {isLoading ? null : (
+        <section>
+          {movies.map((movie) => (
+            <div key={movie.id}>
+              <h4>
+                {movie.title} <small>({movie.year})</small>
+              </h4>
+              <p>{movie.summary}</p>
+              {movie.genres.map((genre, index) => (
+                <li key={index}>{genre}</li>
+              ))}
+              <img src={movie.medium_cover_image} alt="post" />
+            </div>
+          ))}
+        </section>
+      )}
     </div>
   );
 }
